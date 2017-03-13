d6b50cd753309e3d461a15ecaf740b0f2c723046
agusDiyansyah <kreasiin@gmail.com>
2017-03-13 13:28:55 +0700

    update - config database table - tambah informasi table baru

--- application/config/database_table.php
+++ application/config/database_table.php
@@ -3,11 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config["tb_menu_admin"] = "menu_admin";
 $config["tb_pengurus"] = "pengurus";
+$config["tb_galeri"] = "galeri";
+$config["tb_file"] = "file";
+$config["tb_user"] = "user";
+$config["tb_trophy"] = "trophy";
+$config["tb_player"] = "player";
+$config["tb_socmed"] = "socmed";
+$config["tb_jenis_socmed"] = "jenis_socmed";
+$config["tb_team"] = "team";
+$config["tb_match"] = "match";
+
+$config["tb_media"] = "media";
 $config["tb_pemain"] = "pemain";
 $config["tb_jadwal"] = "jadwal";
 $config["tb_hasil_pertandingan"] = "hasil_pertandingan";
 $config["tb_page"] = "page";
-$config["tb_media"] = "media";
-$config["tb_galeri"] = "galeri";
-$config["tb_file"] = "file";
-$config["tb_user"] = "user";
